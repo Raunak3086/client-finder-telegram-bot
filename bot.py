fbf58d05d4fae642111ef7c4a5a7cbad4f61df96
Raunak3086 <deepraunak45@gmail.com>
2026-09-26 08:55:16 +0530

    update 2

--- bot.py
+++ bot.py
@@ -17,6 +17,11 @@ class DummyHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is running!")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
 def keep_alive():
     port = int(os.environ.get('PORT', 10000))
     server = HTTPServer(('0.0.0.0', port), DummyHandler)
